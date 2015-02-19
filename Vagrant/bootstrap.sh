6469d317cb753900722731954e807988552384c3
mateosalta <mateo_salta@yahoo.com>
2015-02-18 18:26:16 -0600

    first

---
+++ Vagrant/bootstrap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y python-pip
+pip install Django
---
+++ Vagrant/startserver.sh
@@ -0,0 +1,4 @@
+
+python /vagrant/my_test_project/manage.py migrate
+echo "Server is started, Navigate to: localhost:8000 in your browser"
+python /vagrant/my_test_project/manage.py runserver 0.0.0.0:8000
