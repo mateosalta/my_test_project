7a0a98550358acef3e674821a622dc577274dc08
mateosalta <mateo_salta@yahoo.com>
2015-02-18 19:18:39 -0600

    whops, fix somthething I started on

--- Vagrant/my_test_project/my_test_project/urls.py
+++ Vagrant/my_test_project/my_test_project/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from testsite.home import views
+
 
 urlpatterns = patterns('',
-    url(r'^$', 'home.views.index')
     # Examples:
     # url(r'^$', 'my_test_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
